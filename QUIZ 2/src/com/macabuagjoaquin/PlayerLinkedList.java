1679b3e5ce204aaa9f80e671e23b5c81bced2b8f
NewGuy45247 <jecmacabuag@gmail.com>
2021-09-23 19:32:49 +0800

    still got some stuff to do

---
+++ QUIZ 2/src/com/macabuagjoaquin/PlayerLinkedList.java
@@ -0,0 +1,52 @@
+package com.macabuagjoaquin;
+
+public class PlayerLinkedList {
+   private PlayerNode head;
+
+// public void removefront(Player player)
+// {
+//     PlayerNode playerNode = playerNode.getNextPlayer(head);
+//     playerNode.getNextPlayer();
+//     head = playerNode;
+// }
+
+    //Adds to the in place counter
+    public int subtractCounter(int x)
+    {
+        x--;
+        System.out.println("Counter: " + x);
+        return x;
+    }
+    //Subtracts from the in place counter
+    public int addCounter(int x)
+    {
+        x++;
+        System.out.println(x);
+        return x;
+    }
+
+    public Player removeHead()
+    {
+        Player remove = head.getPlayer();
+        head = head.getNextPlayer();
+        return remove;
+    }
+
+   public void addtoFront(Player player)
+   {
+       PlayerNode playerNode = new PlayerNode(player);
+       playerNode.setNextPlayer(head);
+       head = playerNode;
+   }
+
+    public void printList()
+    {
+        PlayerNode current = head;
+        System.out.println("HEAD -> ");
+        while (current != null) {
+            System.out.println(current.getPlayer() + "->");
+            current = current.getNextPlayer();
+        }
+        System.out.println("null");
+    }
+}
---
+++ src/com/macabuagjoaquin/Main.java
@@ -0,0 +1,8 @@
+package com.macabuagjoaquin;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
